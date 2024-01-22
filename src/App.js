3876ec4c35054f61c3e77e9069318b31e299aafd
fuwa <fuwafuwa41.glr@gmail.com>
2024-01-22 23:42:07 +0900

    handleClick()を定義

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
 import { useState } from 'react'
 
-function Square(){ // 子
-  const [value, setValue] = useState(null);
-
-  function handleClick(){
-    setValue('X');
-  }
-
+function Square({value, onSquareClick}){ // 引数はprop
   return(
-    <button
-      className="square"
-      onClick={handleClick}
-    >
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   )
 }
 
 export default function Board(){ // 親
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick(){
+    const nextSquares = squares.slice(); //squares配列のコピー
+    nextSquares[0] = "X"
+    setSquares(nextSquares);
+  }
   return (
     <>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={handleClick } />
+      <Square value={squares[1]} onSquareClick={handleClick } />
+      <Square value={squares[2]} onSquareClick={handleClick } />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[3]} onSquareClick={handleClick } />
+      <Square value={squares[4]} onSquareClick={handleClick } />
+      <Square value={squares[5]} onSquareClick={handleClick } />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[6]} onSquareClick={handleClick } />
+      <Square value={squares[7]} onSquareClick={handleClick } />
+      <Square value={squares[8]} onSquareClick={handleClick } />
     </div>
     </>
   )
